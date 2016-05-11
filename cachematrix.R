69656759308356bc428b810925aec063f42a9d4e
Pyrocaster <srinivas.bhaskar@outlook.com>
2016-05-11 15:18:49 +0530

    Comment changes
    Improved readability of comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,43 +1,36 @@
- ## Functions that create a 'special' cached matrix - which is an object stored in the golbal environment  
- ## that wraps around a matrix and can be accessed via access methods. Inverse can be retrieved using the cache solve method
- ## 
+ ## Functions that create and enable a 'special' cached matrix - an object stored in the global environment,
+ ## that wraps around a matrix and can be accessed via access methods.
+ ## Inverse of matrix encapsulated in the 'special' cached matrix object, can be retrieved using the cache solve method
+ 
  
  ## Wrapper that stores matrix and it's inverse in the global environment
  ## returns a list of set, get, setInverse and getInverse
  makeCacheMatrix <- function(x = matrix()) { 
-	 inverse <- NULL
+	inverse <- NULL
 
-	 set <- function(y){
+	set <- function(y){
 		 x <<- y
 		 inverse <<- NULL
-	 }
-
-	 get <- function() {
-	 	x
-	 }
- 
-       setInverse <- function(m_inv){
+	}
+	get <- function() {
+		x
+	}
+        setInverse <- function(m_inv){
 		inverse <<- m_inv
-	 }
-
-	 getInverse <- function(){
+	}
+	getInverse <- function(){
 		inverse
-       }
-	
-       list(set, get, setInverse, getInverse)
+        }
 
+       list(set, get, setInverse, getInverse)
  } 
- 
- 
- 
+
+
  
  ## get the inverse of a 'cache matrix' from global env; if the inverse is not in global env, compute store and return value
- 
- 
  cacheSolve <- function(x, ...) { 
-         ## Return a matrix that is the inverse of 'x' 
-         storedInverse <- x$getInverse()
-
+        ## Return a matrix that is the inverse of 'x' 
+        storedInverse <- x$getInverse()
 		if(!is.null(inverse)){
 			print("getting cached matrix")
 			return (storedInverse)
